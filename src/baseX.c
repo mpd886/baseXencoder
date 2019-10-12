63ba2fff2071a263ccf28e976829d44cac796069
mpd886 <matthewp.donovan@gmail.com>
2019-10-12 19:40:50 -0400

    tests pass

--- src/baseX.c
+++ src/baseX.c
@@ -22,25 +22,30 @@ static const char* _make_base64_alphabet() {
 const char* encode(const char* src) {
     const char* alphabet = _make_base64_alphabet();
     int length = strlen(src) * 8 / 6;
-    char* letters = (char*)calloc(length+1, 1);
+    int padding = strlen(src)%3;
+    char* clear = (char*)calloc(length+padding, 1);
+    strcpy(clear, src); 
+    char* encoded = (char*)calloc(length+padding+1, 1);
     int i = 0;
-    while (i < length) {
-        letters[i++] = alphabet[(*src & 0xf6) >> 2];
 
-        char c = (*src & 0x03) << 4;
-        src++;
-        c |= (*src & 0xf0) >> 4;
-        letters[i++] = alphabet[c];
+    while (i < length+padding) {
+        encoded[i++] = alphabet[(*clear & 0xf6) >> 2];
+
+        char c = (*clear & 0x03) << 4;
+        clear++;
+        c |= (*clear & 0xf0) >> 4;
+        encoded[i++] = alphabet[c];
 
 
-        c = (*src & 0x0f) << 2;
-        ++src;
-        c |= (*src & 0xc0) >> 6;
-        letters[i++] = alphabet[c];
+        c = (*clear & 0x0f) << 2;
+        ++clear;
+        c |= (*clear & 0xc0) >> 6;
+        encoded[i++] = alphabet[c];
 
-        letters[i++] = alphabet[*src & 0x3f];
-        ++src;
+        encoded[i++] = *clear ? alphabet[*clear & 0x3f] : '=';
+        ++clear;
     }
-    return letters;
+
+    return encoded;
 }
 
